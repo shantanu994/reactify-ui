6a34aa53ef0023dc0ef43030921bb9461b9fbfd0
shantanu994 <shantanukulkarni994@gmail.com>
2025-09-13 22:39:27 +0530

    TODO's List

--- todo-list/src/MyComponents/Footer.js
+++ todo-list/src/MyComponents/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light text-center">
+    <footer className="bg-dark text-light text-center" style={{ position: 'absolute', top: '100vh', width: '100%', height: '5vh' }}>
       <p className="text-center">
         Copyright © 2023 My Todos App
         </p>
