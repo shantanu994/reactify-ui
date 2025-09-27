22eeb6d3ec055e2ab98b387d753ce8845f425528
shantanu994 <shantanukulkarni994@gmail.com>
2025-09-27 18:18:13 +0530

    TODO's List

--- todo-list/src/MyComponents/Todos.js
+++ todo-list/src/MyComponents/Todos.js
@@ -1,26 +1,19 @@
 import React from "react";
 import TodoItem from "../MyComponents/TodoItem";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Todos = (props) => {
 
   const [completedTodos, setCompletedTodos] = useState([]);
 
-  const handleCheckboxChange = (todo) => {
-    if (completedTodos.includes(todo.sno)) {
-      setCompletedTodos(completedTodos.filter((sno) => sno !== todo.sno));
-    } else {
-      setCompletedTodos([...completedTodos, todo.sno]);
-    }
-    
-  };
+  let complete = false;
+  function togglecomplete(event){
+    setCompletedTodos(event.target.value);
+  }
 
-  let mystyle = {
-    minHeight: "70vh",
-    margin: "40px auto",
-  };
   return (
-    <div className="container" style={mystyle}>
+    <>
+    <div className="container">
       <h2 className=" my-3">Todos List</h2>
       {props.todos.length === 0
         ? "No Todos to display"
@@ -32,11 +25,17 @@ const Todos = (props) => {
                   key={todo.sno}
                   onDelete={props.onDelete}
                 />
-                <hr />
+                <label>
+                  <input type="radio" value="complete" checked={complete===false} onChange={togglecomplete}  />
+                  complete
+                </label>
+                <p>Complete:{complete }</p>
+                
               </>
             );
           })}
     </div>
+  </>  
   );
 };
 
