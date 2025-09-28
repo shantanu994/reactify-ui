286df12e86afdcccb033f6ae8c5160d8ed108c6e
shantanu994 <shantanukulkarni994@gmail.com>
2025-09-28 20:39:20 +0530

    TODO's List

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -16,6 +16,7 @@ function App() {
 
   const onDelete = (todo) => {
     const newTodos = todos.filter((e) => e !== todo);
+    
     setTodos(newTodos);
     localStorage.setItem("todos", JSON.stringify(newTodos));
   };
